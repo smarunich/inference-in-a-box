4939e0cfdba823b7a4de65a1b606dee36dae81c2
Sergey Marunich <marunich.s@gmail.com>
2025-06-26 20:35:57 -0400

    Update setup-networking.sh

--- inference-in-a-box/scripts/clusters/setup-networking.sh
+++ inference-in-a-box/scripts/clusters/setup-networking.sh
@@ -34,57 +34,7 @@ warn() {
 # Configure cluster networking
 setup_networking() {
     log "Setting up cluster networking..."
-    
-    # Switch to the correct context
-    kubectl config use-context kind-${CLUSTER_NAME}
-    
-    # Create MetalLB for load balancing (required for Istio Gateway)
-    log "Installing MetalLB for load balancing..."
-    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-    kubectl wait --namespace metallb-system --for=condition=ready pod --selector=app=metallb --timeout=300s
-    
-    # Configure MetalLB with docker kind network CIDR
-    log "Configuring MetalLB IP address pool..."
-    
-    # Get the docker kind network CIDR
-    DOCKER_KIND_CIDR=$(docker network inspect kind -f '{{range .IPAM.Config}}{{.Subnet}}{{end}}')
-    
-    # Extract the first three octets of the CIDR
-    PREFIX=$(echo $DOCKER_KIND_CIDR | cut -d'.' -f1-3)
-    
-    # Create MetalLB IP address pool
-    cat <<EOF | kubectl apply -f -
-apiVersion: metallb.io/v1beta1
-kind: IPAddressPool
-metadata:
-  name: default
-  namespace: metallb-system
-spec:
-  addresses:
-  - ${PREFIX}.200-${PREFIX}.250
----
-apiVersion: metallb.io/v1beta1
-kind: L2Advertisement
-metadata:
-  name: default
-  namespace: metallb-system
-EOF
-    
-    # Configure CoreDNS for service discovery
-    log "Configuring CoreDNS for service discovery..."
-    
-    # Add DNS resolution for .local domain
-    kubectl get configmap -n kube-system coredns -o yaml | \
-      sed 's/\/etc\/resolv.conf/\/etc\/resolv.conf\n    forward .local 10.96.0.10/' | \
-      kubectl apply -f -
-    
-    # Restart CoreDNS pods to apply configuration change
-    kubectl delete pod -n kube-system -l k8s-app=kube-dns
-    
-    # Wait for CoreDNS to be ready
-    kubectl wait --namespace kube-system --for=condition=ready pod --selector=k8s-app=kube-dns --timeout=300s
-    
-    success "Cluster networking configured successfully"
+
 }
 
 # Execute networking setup
